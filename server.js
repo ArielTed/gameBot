7762e83058a1065ddd6a7d428d3e1a07e89fc4e0
Ariel Tedgui <atedgui98@gmail.com>
2020-04-01 22:07:57 +0200

    Added info option

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/', express.json({ verify: FBeamer.verifySignature.call(FBeamer) }), (
   return FBeamer.incoming(req, res, async (data) => {
     try {
       if (data.message.text.toLowerCase() === 'info' || data.message.text.toLowerCase() === 'infos' || data.message.text.toLowerCase() === 'information' || data.message.text.toLowerCase() === 'informations') {
-        await FBeamer.txt(data.sender.id, 'Game Bot is a video game chatbot\nYou can ask informations about specific video games, or you can ask for a random game if you don\'t know what to play tonight !\nExample commands: \'What is Fortnite ?\', \'Tell me about PUBG.\', \'What can I play tonight ?\', \'Suggest me a game.\'\nMore infos: https://github.com/ArielTed/gameBot');
+        await FBeamer.txt(data.sender.id, 'Game Bot is a video game chatbot.\nYou can ask informations about specific video games, or you can ask for a random game if you don\'t know what to play tonight !\nExample commands: \'What is Fortnite ?\', \'Tell me about PUBG.\', \'What can I play tonight ?\', \'Suggest me a game.\'\nMore infos: https://github.com/ArielTed/gameBot');
       }
       else {
         const { userData, responseData } = await FBeamer.messageHandler(data);
